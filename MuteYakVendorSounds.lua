2c6ad62bb20dbcaead7387455b58a0d3042fa017
The-Sickness <alloycowboy6969@gmail.com>
2023-07-01 05:24:24 -0500

    Source

---
+++ MuteYakVendorSounds.lua
@@ -0,0 +1,19 @@
+local soundKitIDs = {
+    640336, 640338, 640340,
+    640314, 640316, 640318, 640320,
+    640180, 640182, 640184,
+    640158, 640160, 640162, 640164
+}
+
+local frame = CreateFrame("Frame")
+
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:SetScript("OnEvent", function(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        for _, id in ipairs(soundKitIDs) do
+            MuteSoundFile(id)
+        end
+    end
+end)
+
+
